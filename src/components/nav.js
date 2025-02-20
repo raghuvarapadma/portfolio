aed75a8edaa989a25077fa0f458e862a4ae39110
Raghuvara Padma <raghuvarapadma@gmail.com>
2025-02-20 12:59:01 -0500

    navigation + scaling on website

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,23 +1,55 @@
+"use client";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+
 export default function Nav() {
+    const originalText = "raghu.dev";
+    const [logoText, setLogoText] = useState(originalText);
+    const [hoveringOverLogo, setHoveringOverLogo] = useState(false);
+
+    useEffect(() => {
+        let interval;
+
+        if (hoveringOverLogo) {
+            interval = setInterval(() => {
+                setLogoText(
+                  originalText
+                    .split("")
+                    .map(() => (Math.random() > 0.5 ? "1" : "0"))
+                    .join("")
+                );
+              }, 100);
+        } else {
+            setLogoText(originalText);
+        }
+
+        return () => clearInterval(interval);
+    }, [hoveringOverLogo])
+
     return (
-        <div className="my-4">
+        <div className="font-code my-4">
             <div className="flex items-center justify-between">
-                <div className="font-code flex-1 flex items-center justify-start">
-                    raghu.dev
-                </div>
-                <div className="flex-1 flex items-center justify-end">
-                    <div className="flex gap-7 justify-end">
-                        <div className="flex-1 flex items-center justify-center">
-                            CV
-                        </div>
-                        <div className="flex-1 flex items-center justify-center">
-                            Blog
-                        </div>
-                        <div className="flex-1 flex items-center justify-center">
-                            Hobbies
-                        </div>
+                <div className="flex-1 flex items-center justify-start cursor-pointer transition-all">
+                    <div onMouseEnter={() => setHoveringOverLogo(true)} onMouseLeave={() => setHoveringOverLogo(false)}>
+                        {logoText}
                     </div>
                 </div>
+                <nav className="flex-1 flex items-center justify-end">
+                    <div className="flex gap-7 justify-end">
+                        <Link href="/" className="flex-1 flex items-center justify-center">
+                            home
+                        </Link>
+                        <Link href="/cv" className="flex-1 flex items-center justify-center">
+                            cv
+                        </Link>
+                        <Link href="/blog" className="flex-1 flex items-center justify-center">
+                            blog
+                        </Link>
+                        <Link href="/hobbies" className="flex-1 flex items-center justify-center">
+                            hobbies
+                        </Link>
+                    </div>
+                </nav>
             </div>
         </div>
     );
